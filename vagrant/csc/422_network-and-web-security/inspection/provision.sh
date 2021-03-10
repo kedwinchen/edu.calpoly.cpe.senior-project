3741173b08e53d3bade19b12c3e62bebb4d31523
Kedwin Chen <kedwinchen.noreply+git@gmail.com>
2021-03-10 13:02:33 -0800

    20210310 :: vagrant/csc422-netsec :: add theorycrafted files for building "inspection" box

---
+++ vagrant/csc/422_network-and-web-security/inspection/provision.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -eEuo pipefail
+
+################################################################################
+# Determine where this script is stored
+
+# Solve for the current directory
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+THIS_SCRIPT="${DIR}/$(basename ${0})"
+
+printf "Using (%s) as the path of the provisioner script\n" "${THIS_SCRIPT}"
+
+################################################################################
+# Check permissions
+
+printf "Running as %s (%s)\n" "$(whoami)" "$(id)"
+
+if [[ $EUID -ne 0 ]] ; then
+    printf "%s\n" "Attempting to elevate..."
+    # re-launch as root
+    exec sudo -i bash "${THIS_SCRIPT}"
+fi
+
+# this should not be able to occur
+if [[ $EUID -ne 0 ]] ; then
+    # at this point, we should be running as root
+    # if not, then using `sudo` failed
+    printf "%s\n" "Could not elevate to root"
+    exit 1
+else
+    printf "%s\n" "Successfully elevated privileges"
+fi
+
+################################################################################
+# Provisioner part
+
+apt-get update
+apt-get upgrade -y
+
+apt-get install -y snort iptables
