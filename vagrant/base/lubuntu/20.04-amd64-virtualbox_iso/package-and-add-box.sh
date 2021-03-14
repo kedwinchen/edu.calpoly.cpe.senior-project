bb5ef24d736c8ec678e35812734be3e0bfba52c1
Kedwin Chen <kedwinchen.noreply+git@gmail.com>
2021-03-14 12:26:19 -0700

    20210314 :: vagrant/csc422-netsec :: update lubuntu box, add metadata
    - metadata allows compatibility with Vagrant cloud

--- vagrant/base/lubuntu/20.04-amd64-virtualbox_iso/package-and-add-box.sh
+++ vagrant/base/lubuntu/20.04-amd64-virtualbox_iso/package-and-add-box.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eEuo pipefail
+set -xeEuo pipefail
 
 readonly OS_NAME="lubuntu"
 readonly OS_VERSION="20.04"
@@ -12,14 +12,14 @@ readonly BOX_NAME="${BASE_NAME}.box"
 if [[ -e "${BOX_NAME}" ]]; then
     if [[ -f "${BOX_NAME}" ]]; then
         rm -vf "${BOX_NAME}"
+        vagrant box remove "${BASE_NAME}"
     else
         printf 'Could not remove existing output file: %s\n' "${BOX_NAME}"
         exit 1
     fi
 fi
 
-set -x
-vagrant box remove "${BASE_NAME}"
-vagrant package --base "${BASE_NAME}" --output "${BOX_NAME}"
+vagrant package --base "${BASE_NAME}" --output "${BOX_NAME}" --include "info.json"
 vagrant box add --force --name "${BASE_NAME}" "${BOX_NAME}"
+sha512sum --tag "${BOX_NAME}" | tee CHECKSUMS.sha512
 set +x
